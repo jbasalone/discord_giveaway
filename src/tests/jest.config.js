e944e6aa919fd89d6c4b9e7b259e62991aa04c02
Jenn Basalone <jbasalone@gmail.com>
2025-02-13 22:55:36 -0800

    removing tests

--- src/tests/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], // Loads the test setup
-    testTimeout: 30000, // Extend timeout if database queries are slow
-    verbose: true, // Display detailed test output
-    detectOpenHandles: true, // Helps track hanging processes
-    forceExit: true, // Ensures Jest exits after running tests
-};
